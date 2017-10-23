65b031154f20bb041a1456a985bc8cfd544310fc
yordanivanov92 <92yordanivanov@gmail.com>
2017-10-23 23:32:01 +0200

    Added NN with cross-entropy error and using a command for more detailed ConfusionMatrix

--- bank_sim_models/bank_sim_NN.R
+++ bank_sim_models/bank_sim_NN.R
@@ -2,6 +2,7 @@
 library(nnet)
 library(neuralnet)
 library(caTools)
+library(caret)
 bankSim <- read.csv(file = "C:/Users/Yordan Ivanov/Desktop/Master Thesis Project/data/bank_sim_synthetic/bs140513_032310.csv",
                     header = TRUE,
                     sep = ",")
@@ -25,8 +26,8 @@ train = subset(bankSim_model, split == TRUE)
 test = subset(bankSim_model, split == FALSE)
 
 # Fitting a simple NN with 1 hidden layer
-nn1 <- nnet(fraud~age+gender+category+amount,data = train, size = 10)
-plot(nn1)
+#nn1 <- nnet(fraud~age+gender+category+amount,data = train, size = 10)
+#plot(nn1)
 
 # Fitting NN with more hidden layers layers
 m <- model.matrix( 
@@ -39,8 +40,8 @@ colnames(m) <- c("Intercept","fraud",depvars)
 f <- paste(depvars, collapse = ' + ')
 f <- paste('fraud ~', f)
 f <- as.formula(f)
-nn <- neuralnet(f, m, hidden=c(10,10),linear.output=FALSE)
-plot(nn)
+nn <- neuralnet(f, m, hidden=c(10),linear.output=FALSE)
+#plot(nn)
 
 # Fitting the trained NN model to the test dataset
 m2 <- model.matrix( 
@@ -56,4 +57,23 @@ predTest <- ifelse(predTest>=0.5,
                    1,
                    0)
 # Getting the confusion matrix
-table(predTest, m2[,"fraud"])
+predict_table <- table(predTest, m2[,"fraud"])
+confusionMatrix(predict_table)
+#nn <- neuralnet(f, m, hidden=c(10),linear.output=FALSE)
+#predTest    0    1
+#       0 3942   18
+#       1   13   27
+
+#nn <- neuralnet(f, m, hidden=c(10,10),linear.output=FALSE)
+#predTest    0    1
+#       0 3936   22
+#       1   16   26
+# 16 False positives (in reality are not fraud but are classified as fraud by our algorithm)
+# 22 False negatives (in reality are fraud but are classified as not fraud by our algorithm)
+# 26 fraud case properly classified
+# 3936 regular transactions properly classifed
+
+#nn <- neuralnet(f, m, hidden=c(10,10,10),linear.output=FALSE)
+#predTest    0    1
+#       0 3929   17
+#       1   26   28
